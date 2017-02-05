902582306e0a9b7d29ab3e52a829b27270db2dd3
Benoit deKajoo <dekajoo@gmail.com>
2017-02-06 00:16:23 +0100

    fix heroku

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const app = express();
 
 // If an incoming request uses a protocol other than HTTPS,
 // redirect that request to the same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
+//const forceSSL = function() {
+//  return function (req, res, next) {
+//    if (req.headers['x-forwarded-proto'] !== 'https') {
+//      return res.redirect(
+//       ['https://', req.get('Host'), req.url].join('')
+//      );
+//    }
+//    next();
+//  }
+//}
 // Instruct the app to use the forceSSL middleware
-app.use(forceSSL());
+//app.use(forceSSL());
 
 // Gzip middleware
 app.use(compression());
