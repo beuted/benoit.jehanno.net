220aebc2533c4fcb80af424e055a242558450afd
Benoit deKajoo <dekajoo@gmail.com>
2017-02-05 23:15:08 +0100

    Added mansory for articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TextCarouselComponent } from './text-carousel/text-carousel.component';
+import { MasonryOptions } from 'angular2-masonry';
 import * as _ from "lodash";
 
 @Component({
@@ -9,10 +10,55 @@ import * as _ from "lodash";
 })
 export class AppComponent {
   private likedStuff = [
-    'Nethack', 'UnNethack', 'New Techs', 'Reddit', '🌽', '🍣', '🍺', 'ES6', 'Typescript', 'Nodejs', 'C#', 'Java' 'Esport', 'Hiking', '🏔 Mountains',
+    'Nethack', 'UnNethack', 'New Techs', 'Reddit', '🌽', '🍣', '🍺', 'ES6', 'Typescript', 'Nodejs', 'C#', 'Java', 'Esport', 'Hiking', '🏔 Mountains',
     'Hackernews', 'Coding games', 'Coding small projects', '🕹 Indie games', 'All sorts of musics', '🎲 Board games'
   ];
   public likedStuffSuffled = _.sampleSize(this.likedStuff, this.likedStuff.length);
+
+  public myOptions: MasonryOptions = {
+    transitionDuration: '0.8s'
+  };
+
+  public bricks = [
+     {
+        title: 'JS RPG',
+        github: 'https://github.com/beuted/JS-RPG',
+        demo: 'https://js-rpg.herokuapp.com/index.html',
+        image: './assets/images/projects/js-rpg.PNG',
+        height: 300,
+        date: '2011'
+     },
+     {
+        title: 'WEBGL FPS',
+        github: 'https://github.com/beuted/WebGL-FPS',
+        demo: 'https://webgl-fps.herokuapp.com/src/index.html',
+        image: './assets/images/projects/webgl-fps.PNG',
+        height: 300,
+        date: '2012'
+     },
+     {
+        title: 'smoothycon',
+        github: 'https://github.com/beuted/smoothycon',
+        demo: 'https://smoothycon.herokuapp.com/demo.html',
+        image: './assets/images/projects/smoothycon.PNG',
+        height: 300,
+        date: 'March 2015'
+     },
+     {
+        title: 'ng-chocolat',
+        github: 'https://github.com/beuted/ng-chocolat',
+        image: './assets/images/projects/ng-chocolat.PNG',
+        height: 300,
+        date: 'September 2015'
+     },
+     {
+        title: 'csgo-mates.com',
+        github: 'http://csgo-mates.com',
+        image: './assets/images/projects/csgo-mates.PNG',
+        height: 500,
+        date: '2013-2014'
+     },
+   ];
   
   constructor() {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,21 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MasonryModule } from 'angular2-masonry';
 
 import { AppComponent } from './app.component';
 import { TextCarouselComponent } from './text-carousel/text-carousel.component';
+import { BricksComponent } from './bricks/bricks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TextCarouselComponent
+    TextCarouselComponent,
+    BricksComponent
   ],
   imports: [
     NgbModule.forRoot(),
+    MasonryModule,
     BrowserModule,
     FormsModule,
     HttpModule
