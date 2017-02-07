826c1dda3797cb69c3737b0059b7243def73fea3
Benoit deKajoo <dekajoo@gmail.com>
2017-02-07 23:48:58 +0100

    improve style and intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TextCarouselComponent } from './text-carousel/text-carousel.component';
 import { MasonryOptions } from 'angular2-masonry';
+import { PageScrollConfig } from 'ng2-page-scroll';
 import * as _ from "lodash";
 
 @Component({
@@ -92,6 +93,15 @@ export class AppComponent {
    ];
   
   constructor() {
+    PageScrollConfig.defaultEasingLogic = {
+            ease: (t: number, b: number, c: number, d: number): number => {
+                // easeInOutExpo easing
+                if (t === 0) return b;
+                if (t === d) return b + c;
+                if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+                return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+            }
+        };
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MasonryModule } from 'angular2-masonry';
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
 
 import { AppComponent } from './app.component';
 import { TextCarouselComponent } from './text-carousel/text-carousel.component';
@@ -18,6 +19,7 @@ import { BricksComponent } from './bricks/bricks.component';
   imports: [
     NgbModule.forRoot(),
     MasonryModule,
+    Ng2PageScrollModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
