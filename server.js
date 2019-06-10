62773c946dee26e5a1703575695d027e815ddf2e
Benoit Dekajoo <dekajoo@gmail.com>
2019-06-10 13:00:00 +0200

    Add verbosity to statup

--- server.js
+++ server.js
@@ -29,4 +29,7 @@ app.get('/*', function(req, res) {
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+let port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log('benoit.jehanno.net is running at localhost:' + port);
